be62afad860592f9d7cbc41391d7b0d922ed8131
Johurul Hassan Mazumder <johurulhassan000@gmail.com>
2024-07-08 12:47:22 +0530

    Initial commit

---
+++ backend/users/serializers.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        field = ['id', 'username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+        def create(self, validated_data):
+            user = User.objects.create_user(**validated_data)
+            return user
+        
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only = True)
+    
